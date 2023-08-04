ff107ce63f3b95bbc37ef71457f43b052b246c42
alek <wait82985@gmail.com>
2023-08-04 18:58:15 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,63 @@
 public class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        //TODO
-        return null;
+        int m  = mat.length;
+        int n = mat[0].length;
+
+        int left = 0;
+        int right = n - 1;
+        int mid = left + (right - left) / 2;
+
+        while (left <= right){
+            mid = left + (right - left) / 2;
+            int[] coordsMax = findMax(mid, mat);
+            if(coordsMax[1] == mid){
+                return coordsMax;
+            }
+
+            if(coordsMax[1] < mid){
+                right = mid - 1;
+            }
+            if(coordsMax[1] > mid){
+                left = mid + 1;
+            }
+        }
+
+
+
+
+        return new int[]{-1, -1};
+    }
+    public int[] findMax(int indexJ, int[][] mat){
+        int max = 0;
+        int columnWithMax = - 1;
+        int indexI = -1;
+
+        for (int i = 0; i < mat.length; i++) {
+            if(mat[i][indexJ] > max){
+                max = mat[i][indexJ];
+                columnWithMax = indexJ;
+                indexI= i;
+            }
+        }
+        if(indexJ != 0){
+            for (int i = 0; i < mat.length; i++) {
+                if(mat[i][indexJ - 1] > max){
+                    max = mat[i][indexJ - 1];
+                    columnWithMax = indexJ - 1;
+                    indexI= i;
+                }
+            }
+        }
+        if(indexJ != mat[0].length - 1){
+            for (int i = 0; i < mat.length; i++) {
+                if(mat[i][indexJ + 1] > max){
+                    max = mat[i][indexJ];
+                    columnWithMax = indexJ + 1;
+                    indexI= i;
+                }
+            }
+        }
+
+        return new int[]{indexI, columnWithMax};
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -11,7 +11,7 @@ public class SolutionTest {
         int[] expected = {0, 1};
         int[] actual = new Solution().findPeakGrid(mat);
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertArrayEquals(expected, actual);
     }
 
     @Test
@@ -24,6 +24,36 @@ public class SolutionTest {
         int[] expected = {1, 1};
         int[] actual = new Solution().findPeakGrid(mat);
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void findPeakGridTest3() {
+        int[][] mat = {
+                {41, 8, 2, 48, 18},
+                {16, 15, 9, 7, 44},
+                {48, 35, 6, 38, 28},
+                {3, 2, 14, 15, 33},
+                {39, 36, 13, 46, 42}
+
+        };
+        int[] expected = {0, 3};
+        int[] actual = new Solution().findPeakGrid(mat);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findPeakGridTest4() {
+        int[][] mat = {
+                {10,50,40,30,20},
+                {1,500,2,3,4}
+
+        };
+        int[] expected = {1, 1};
+        int[] actual = new Solution().findPeakGrid(mat);
+
+        Assert.assertArrayEquals(expected, actual);
     }
 }
